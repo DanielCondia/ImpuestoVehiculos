b579591f5836266c060899e6eae86799aaac0e0d
Daniel Condía Figueredo <daniel.condia@gmail.com>
2023-03-27 18:08:04 -0500

    Se crea una interfaz por aparte para dejar el main libre

---
+++ gui/Principal.java
@@ -0,0 +1,9 @@
+package co.edu.uptc.gui;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		InterfazPrincipal principal = new InterfazPrincipal();
+		principal.setVisible(true);
+	}
+}
