02eb93e97ddb6048819faf860572abbbda0e904b
Daniel Condía Figueredo <daniel.condia@gmail.com>
2023-03-27 20:12:53 -0500

    Se agregan y se cuadran todas las etiquetas de los datos del propietario, se dejan listos los dos paneles para cuadrarlos en los extremos del JFrame

--- gui/InterfazPrincipal.java
+++ gui/InterfazPrincipal.java
@@ -1,18 +1,75 @@
 package co.edu.uptc.gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class InterfazPrincipal extends JFrame {
-	JPanel panel = new JPanel();
+	private static final JPanel panel = new JPanel(), panel2 = new JPanel();
+	public JTextField inputPrimerNombre, inputSegundoNombre, inputApellidos, inputNumeroDoc;
 	public InterfazPrincipal(){
+		//Colocamos el metodo borderLayout al JFrame
+		BorderLayout layout = new BorderLayout();
+		setLayout(layout);
+		/*
+		* Hacemos que la pagina se centre
+		* Le damos dimensiones a la pagina
+		* Agregamos que se cierre cuando se le de a la x
+		* */
 		setLocationRelativeTo(null);
 		setBounds(200,100,1000,600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//Le damos titulo a la interfaz
 		setTitle("Propietarios de vehiculos");
-		add(panel);
-		panel.setLayout(new GridLayout(4,3,5,5) );
+		/*
+		* Se agrega el primer panel en la parte izquierda del JFrame, con el metodo West
+		* Gracias al BorderLayout antes creado
+		* */
+		add(panel, BorderLayout.WEST);
 
+		//Titulo de la sección de datos del propietario
+		JLabel label = new JLabel("Datos del propietario");
+		label.setBounds(10,10,200,20);
+		panel.add(label);
 
+		//Texto para introducir el primer nombre
+		JLabel textInputNombre = new JLabel("(*)Primer nombre:");
+		textInputNombre.setBounds(12,50,140,20);
+		panel.add(textInputNombre);
+
+		//Espacio para que el usuario ingrese el primer nombre
+		inputPrimerNombre = new JTextField();
+		inputPrimerNombre.setBounds(120,50,150,20);
+		panel.add(inputPrimerNombre);
+
+		//Texto para introducir el segundo nombre
+		JLabel textInputSegundoNombre =  new JLabel("Segundo nombre:");
+		textInputSegundoNombre.setBounds(12,100,100,20);
+		panel.add(textInputSegundoNombre);
+
+		//Espacio para que el usuario ingrese el segundo nombre
+		inputSegundoNombre = new JTextField();
+		inputSegundoNombre.setBounds(120,100,150,20);
+		panel.add(inputSegundoNombre);
+
+		//Texto para introducir apellidos
+		JLabel textInputApellidos = new JLabel("(*)Apellidos:");
+		textInputApellidos.setBounds(12,160,100,20);
+		panel.add(textInputApellidos);
+
+		//Espacio para ingresar apellidos
+		inputApellidos = new JTextField();
+		inputApellidos.setBounds(120,160,150,20);
+		panel.add(inputApellidos);
+
+		//Texto para ingresar número de documento
+		JLabel inputNumeroDocumento = new JLabel("(*)Número Doc:");
+		inputNumeroDocumento.setBounds(12,220,140,20);
+		panel.add(inputNumeroDocumento);
+
+		//Espacio para ingresar el número de documento
+		inputNumeroDoc = new JTextField();
+		inputNumeroDoc.setBounds(120,220,150,20);
+		panel.add(inputNumeroDoc);
 	}
 }
