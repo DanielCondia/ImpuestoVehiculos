6ca853ec7dc4eafe9d72df9f314f35dcc8f27d20
Daniel Condía Figueredo <daniel.condia@gmail.com>
2023-03-28 10:11:46 -0500

    Se agregan todas las etiquetas dentro del JFrame, se logra cuadrar los dos paneles uno al lado del otro, todas las etiquetas de entrada ya estab cuadradas, tal cual como la imagen de plataforma, solo falta crear los botones y cuadrarlos

--- gui/InterfazPrincipal.java
+++ gui/InterfazPrincipal.java
@@ -6,11 +6,30 @@ import java.awt.*;
 
 public class InterfazPrincipal extends JFrame {
 	private static final JPanel panel = new JPanel(), panel2 = new JPanel();
-	public JTextField inputPrimerNombre, inputSegundoNombre, inputApellidos, inputNumeroDoc;
+	//Datos del propietario
+	public JTextField inputPrimerNombre, inputSegundoNombre, inputApellidos, inputNumeroDoc, inputGenero;
+	//Datos del vehiculo
+	public JTextField inputColor, inputPlaca, inputModelo, inputValorComercial, inputImpuestos;
 	public InterfazPrincipal(){
-		//Colocamos el metodo borderLayout al JFrame
-		BorderLayout layout = new BorderLayout();
-		setLayout(layout);
+		/*
+		* Damos dimensiones con el gridLayout para que el primer y segundo panel
+		* queden como una tabla
+		* */
+		LayoutManager layout = new GridLayout(5,3,2,2);
+
+		/*
+		* Ahora con BorderLayout, cuadramos un panel a la izquierda y otro a la derecha
+		* */
+		LayoutManager posicion = new BorderLayout();
+
+		//Damos dimensión de tabla al primer panel
+		panel.setLayout(layout);
+
+		//Damos dimensión de tabla al segundo panel
+		panel2.setLayout(layout);
+
+		//Agregamos las posiciones al JFrame
+		setLayout(posicion);
 		/*
 		* Hacemos que la pagina se centre
 		* Le damos dimensiones a la pagina
@@ -26,50 +45,105 @@ public class InterfazPrincipal extends JFrame {
 		* Gracias al BorderLayout antes creado
 		* */
 		add(panel, BorderLayout.WEST);
-
-		//Titulo de la sección de datos del propietario
-		JLabel label = new JLabel("Datos del propietario");
-		label.setBounds(10,10,200,20);
-		panel.add(label);
+		//Se agrega el segundo panel en la parte derecha del JFrame, con el metodo East
+		add(panel2, BorderLayout.EAST);
+		//Cuadramos la pagina para que se ajuste a las dimensiones
+		pack();
+		setSize(500,200);
+		/*
+		* Datos de la sección del propietario
+		* Agregamos el titulo a la sección.
+		* */
+		Border bordePropietario = BorderFactory.createTitledBorder("Datos de Propietario");
+		panel.setBorder(bordePropietario);
 
 		//Texto para introducir el primer nombre
 		JLabel textInputNombre = new JLabel("(*)Primer nombre:");
-		textInputNombre.setBounds(12,50,140,20);
 		panel.add(textInputNombre);
 
 		//Espacio para que el usuario ingrese el primer nombre
 		inputPrimerNombre = new JTextField();
-		inputPrimerNombre.setBounds(120,50,150,20);
 		panel.add(inputPrimerNombre);
 
 		//Texto para introducir el segundo nombre
 		JLabel textInputSegundoNombre =  new JLabel("Segundo nombre:");
-		textInputSegundoNombre.setBounds(12,100,100,20);
 		panel.add(textInputSegundoNombre);
 
 		//Espacio para que el usuario ingrese el segundo nombre
 		inputSegundoNombre = new JTextField();
-		inputSegundoNombre.setBounds(120,100,150,20);
 		panel.add(inputSegundoNombre);
 
 		//Texto para introducir apellidos
 		JLabel textInputApellidos = new JLabel("(*)Apellidos:");
-		textInputApellidos.setBounds(12,160,100,20);
 		panel.add(textInputApellidos);
 
 		//Espacio para ingresar apellidos
 		inputApellidos = new JTextField();
-		inputApellidos.setBounds(120,160,150,20);
 		panel.add(inputApellidos);
 
 		//Texto para ingresar número de documento
 		JLabel inputNumeroDocumento = new JLabel("(*)Número Doc:");
-		inputNumeroDocumento.setBounds(12,220,140,20);
 		panel.add(inputNumeroDocumento);
 
 		//Espacio para ingresar el número de documento
 		inputNumeroDoc = new JTextField();
-		inputNumeroDoc.setBounds(120,220,150,20);
 		panel.add(inputNumeroDoc);
+
+		//Texto para ingresar el genero
+		JLabel textGenero = new JLabel("Genero(m/f):");
+		panel.add(textGenero);
+
+		//Espacio para ingresar el genero
+		inputGenero = new JTextField();
+		panel.add(inputGenero);
+
+		/*
+		 * Sección de los datos del carro
+		 * Agregamos titulo a la sección de los datos del carro
+		 * */
+
+		Border bordeVehiculo = BorderFactory.createTitledBorder("Datos de Vehículo");
+		panel2.setBorder(bordeVehiculo);
+
+		//Texto para ingresar Color del vehiculo
+		JLabel textColor = new JLabel("Color:");
+		panel2.add(textColor);
+
+		//Espacio para ingresar el color del vehiculo
+		inputColor = new JTextField();
+		panel2.add(inputColor);
+
+		//Texto para ingresar el número de placa
+		JLabel textPlaca = new JLabel("(*)Número de placa:");
+		panel2.add(textPlaca);
+
+		//Espacio para ingresar la serie de la placa
+		inputPlaca = new JTextField();
+		panel2.add(inputPlaca);
+
+		//Texto para ingresar modelo del vehiculo
+		JLabel textModelo = new JLabel("(*)Modelo:");
+		panel2.add(textModelo);
+
+		//Espacio para ingresar el modelo del vehiculo
+		inputModelo = new JTextField();
+		panel2.add(inputModelo);
+
+		//Texto para el valor comercial
+		JLabel textValor = new JLabel("(*)Valor comercial:");
+		panel2.add(textValor);
+
+		//Espacio para ingresar el valor comercial
+		inputValorComercial = new JTextField();
+		panel2.add(inputValorComercial);
+
+		//Texto para tarifa de impuestos
+		JLabel textImpuestos = new JLabel("(*)Tarifa de impuestos:");
+		panel2.add(textImpuestos);
+
+		//Espacio para ingresar los impuestos
+		inputImpuestos = new JTextField();
+		panel2.add(inputImpuestos);
+
 	}
 }
