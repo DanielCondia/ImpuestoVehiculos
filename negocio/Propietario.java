7e98418b68965899cb75b08aa6bdb6d0f407e34a
Daniel Condía Figueredo <daniel.condia@gmail.com>
2023-03-29 10:55:48 -0500

    Se programa la clase de propietario y la clase vehiculo, todo funciona bien, y se crea nueva clase BaseDatos, para guardar los datos recolectados en propietario

--- negocio/Propietario.java
+++ negocio/Propietario.java
@@ -1,4 +1,27 @@
 package co.edu.uptc.negocio;
 
-public class Propietario {
+import co.edu.uptc.gui.InterfazPrincipal;
+
+public class Propietario extends InterfazPrincipal {
+	public String primerNombre, segundoNombre, apellido, genero;
+	public long numeroDocumento;
+	public Vehiculo vehiculo;
+	public Propietario(){
+		this.primerNombre = inputPrimerNombre.getText();
+		this.segundoNombre = inputSegundoNombre.getText();
+		this.apellido = inputApellidos.getText();
+		this.genero = inputGenero.getText();
+		this.numeroDocumento = Long.parseLong(inputNumeroDoc.getText());
+	}
+	@Override
+	public String toString() {
+		return "Propietario{" +
+				"primerNombre='" + primerNombre + '\'' +
+				", segundoNombre='" + segundoNombre + '\'' +
+				", apellido='" + apellido + '\'' +
+				", genero='" + genero + '\'' +
+				", numeroDocumento=" + numeroDocumento +
+				", vehiculo=" + vehiculo.toString() +
+				'}';
+	}
 }
--- negocio/Vehiculo.java
+++ negocio/Vehiculo.java
@@ -1,4 +1,31 @@
 package co.edu.uptc.negocio;
 
-public class Vehiculo {
+import co.edu.uptc.gui.InterfazPrincipal;
+
+public class Vehiculo extends InterfazPrincipal {
+	public String color, placa, modelo;
+	public long valorComercial, impuestos;
+	public Vehiculo(){
+		this.color = inputColor.getText();
+		this.placa = inputPlaca.getText();
+		this.modelo = inputModelo.getText();
+		this.valorComercial = Long.parseLong(inputValorComercial.getText());
+		this.impuestos = Long.parseLong(inputImpuestos.getText());
+	}
+	@Override
+	public String toString() {
+		return "Vehiculo{" +
+				"color='" + color + '\'' +
+				", placa='" + placa + '\'' +
+				", modelo='" + modelo + '\'' +
+				", TotalImpuestos=" + totalImpuestos() +
+				", ValorTotal=" + totalValor() +
+				'}';
+	}
+	public long totalImpuestos(){
+		return valorComercial * impuestos / 100;
+	}
+	public long totalValor(){
+		return valorComercial + totalImpuestos();
+	}
 }
